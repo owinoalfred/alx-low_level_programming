3c84cdf946acf98aed21fbdd128075955f2bbd02
owinoalfred <ogzialfred@gmail.com>
2023-03-16 06:48:14 -0700

    yet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- * File: 4-puts.c
- * Auth: alfred owino
- * main : Entry point
- * Return : Always 0 (Success)
- * Desc: A program that prints Programming is like a multilingual puzzle
- */
+* main - Entry point
+* Return : Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
