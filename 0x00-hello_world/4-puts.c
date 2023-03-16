190040e16ff0cb62dc2a1eb85f91f2f9730b70de
owinoalfred <ogzialfred@gmail.com>
2023-03-16 06:05:18 -0700

    a file that uses puts to print Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-int main(){
-	puts("\"Programming is like building a multilingual puzzle");
-return 0;
+/*
+ * File: 4-puts.c
+ * Auth: alfred owino
+ * Desc: A program that prints Programming is like a multilingual puzzle
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
 }
+
