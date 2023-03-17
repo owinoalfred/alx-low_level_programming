1a0efd90c39c2bec2ad9ac7c99ce80722babba7d
owinoalfred <ogzialfred@gmail.com>
2023-03-17 15:05:21 -0700

    TIS EVALUATES THE LAST DIGITS

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+int last_digit = n % 10;
+
+printf("Last digit of %d is ", n);
+
+if (last_digit > 5)
+{
+printf("%d and is greater than 5\n", last_digit);
+}
+else if (last_digit == 0)
+{
+printf("%d and is 0\n", last_digit);
+}
+else
+{
+printf("%d and is less than 6 and not 0\n", last_digit);
+}
+
+return (0);
+}
+
