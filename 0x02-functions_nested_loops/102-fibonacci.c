eca6a98a71869135ccc86866050554dada3520ef
owinoalfred <ogzialfred@gmail.com>
2023-03-21 11:57:11 -0700

    fibonacci   func

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-
 /**
- * main - prints the sum of all the multiples of 3 or 5 below 1024
- *
- * Return: Always 0.
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, sum = 0;
+int i, sum = 0; 
 for (i = 0; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
+{
 sum += i;
 }
-printf("%d\n", sum);
+}    
+printf("%d\n", sum);    
 return (0);
 }
