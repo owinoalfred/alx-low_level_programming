5309f6548ac05ccdf29571b87d46f5cf8f2c71af
owinoalfred <ogzialfred@gmail.com>
2023-03-21 12:01:30 -0700

    this  func loops through

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,15 +5,14 @@
  */
 int main(void)
 {
-int i, sum = 0; 
+int i, sum = 0;
 for (i = 0; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
 sum += i;
 }
-}    
-printf("%d\n", sum);    
+}
+printf("%d\n", sum);
 return (0);
 }
-
