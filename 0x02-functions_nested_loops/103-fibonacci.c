d1e10fbb5fc115a92466a80279cefd952da89224
owinoalfred <ogzialfred@gmail.com>
2023-03-21 12:20:33 -0700

    correctiing thte mixed declarations3s

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,13 +5,14 @@
  */
 int main() {
 int prev_num = 1;
+int next_num;
 int curr_num = 2;
 int sum_even = 0;
 while (curr_num <= 4000000) {
 if (curr_num % 2 == 0) {
 sum_even += curr_num;
 }
-int next_num = prev_num + curr_num;
+next_num = prev_num + curr_num;
 prev_num = curr_num;
 curr_num = next_num;
 }
