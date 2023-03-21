91915c5786cd17ecd13693810356aea70037bc8c
owinoalfred <ogzialfred@gmail.com>
2023-03-21 12:13:22 -0700

    fibonnacii  patterns

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints the first 50 Fibonacci numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n, i, t1 = 1, t2 = 2, nextTerm;
-printf("First 50 Fibonacci numbers: ");
-for (i = 1; i <= 50; i++)
+int i;
+long int num1 = 1, num2 = 2, fib;
+printf("%ld, %ld", num1, num2);
+for (i = 2; i < 50; i++)
 {
-printf("%d", t1);
-if (i < 50)
-{
-printf(", ");
-}
-nextTerm = t1 + t2;
-t1 = t2;
-t2 = nextTerm;
+fib = num1 + num2;
+printf(", %ld", fib);
+num1 = num2;
+num2 = fib;
 }
 printf("\n");
-return 0;
+return (0);
 }
