5b1fd5d843fed701933a4866cbbf4e3b0f500bd6
owinoalfred <ogzialfred@gmail.com>
2023-03-21 09:41:20 -0700

    putchar in the sec file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
