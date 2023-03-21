40206b4316aa17e642e8cb35b89ec60cc4562f19
owinoalfred <ogzialfred@gmail.com>
2023-03-21 11:38:09 -0700

    prints 100 times  func

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,29 +4,11 @@
  * print_times_table - prints the n times table, 
  * @n: the times table to print
  */
-void print_times_table(int n)
-{
-int i, j;
-if (n < 0 || n > 15)
-return;
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-if (j == 0)
-_putchar('0');
-else
-{
-int res = i * j;
-_putchar(',');
-_putchar(' ');
-if (res < 10)
-_putchar(' ');
-else if (res < 100)
-_putchar('0' + res / 10);
-_putchar('0' + res % 10);
+void print_times_table(int n) {
+for (int i = 0; i <= n; i++) {
+for (int j = 0; j <= n; j++) {
+printf("%-4d", i*j); 
 }
-}
-_putchar('\n');
+printf("\n");
 }
 }
