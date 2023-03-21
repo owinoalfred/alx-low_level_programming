88cd57f80c1da74de9807ae38ad59d9cc7f3f495
owinoalfred <ogzialfred@gmail.com>
2023-03-21 12:18:30 -0700

    fibonnacii 0x03s

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main() {
+int prev_num = 1;
+int curr_num = 2;
+int sum_even = 0;
+while (curr_num <= 4000000) {
+if (curr_num % 2 == 0) {
+sum_even += curr_num;
+}
+int next_num = prev_num + curr_num;
+prev_num = curr_num;
+curr_num = next_num;
+}
+printf("%d\n", sum_even);
+return (0);
+}
