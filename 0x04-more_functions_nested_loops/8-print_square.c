38dd3c7e518959304bd2aecc7099c10ee71b4f12
owinoalfred <ogzialfred@gmail.com>
2023-03-23 06:45:43 -0700

    prints numbers in c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square using the # character
+ * @size: Size of the square
+ */
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar('#');
+_putchar('\n');
+}
+}
