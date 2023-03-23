775d2297625f2c33203eab605b7164f63f23ca06
owinoalfred <ogzialfred@gmail.com>
2023-03-23 07:15:59 -0700

    this is the fizz buzz func

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,24 @@
-i#include "main.h"
+#include "main.h"
 
 /**
- * print_triangle - prints a triangle of '#' characters
- * @size: size of the triangle to be printed
- *
- * Return: void
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
  */
+
 void print_triangle(int size)
 {
+int i, j;
 if (size <= 0)
 {
 _putchar('\n');
+return;
 }
-else
-{
-int i, j;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size - i - 1; j++)
-{
 _putchar(' ');
-}
 for (j = 0; j <= i; j++)
-{
 _putchar('#');
-}
 _putchar('\n');
-}
 }
 }
