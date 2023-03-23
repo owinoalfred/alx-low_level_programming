d93119cc8688460991c45a8cae45cc9d6d94b0ef
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-23 18:25:16 +0300

    Update 9-fizz_buzz.c
    fiz buz altereed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Prints the first 50 Fibonacci numbers
- * Return: Always 0 (Success)
+ * main - prints the numbers 1 - 100,
+ *  instead of multiples of 3, print Fizz
+ *  instead of multiples of 5, print Buzz
+ *  for multiples of 3 and 5, print FizzBuzz.
+ * Return: 0
  */
 int main(void)
 {
@@ -11,18 +13,24 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
-printf("%d ", i);
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
 }
 printf("\n");
 return (0);
