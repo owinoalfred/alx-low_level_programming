c161adcb3d9c3c0f9de3073b6bd2f7cad8e75ac5
owinoalfred <ogzialfred@gmail.com>
2023-03-23 06:36:47 -0700

    prints numbers in c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _putchar(char c);
 int _isdigit(int c);
 void print_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 void print_most_numbers(void);
 int mul(int a, int b);
 #endif
