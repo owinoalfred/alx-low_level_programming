fc9f7e81efce1d82aaeb6f27510c308c9a1978d8
owinoalfred <ogzialfred@gmail.com>
2023-03-27 08:59:40 -0700

    lenbnth of t he

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string, followed by a new line, to stdout.
- * @str: The string to be printed.
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer to the string to print
  *
- * Return: void.
+ * Return: void
  */
 void _puts(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
+int i = 0;
+while (str[i] != '\0')
 {
 _putchar(str[i]);
+i++;
 }
 _putchar('\n');
 }
