ee3ebf04c2e1fd4d1b594c74da7faf693dbcf669
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-28 01:02:35 +0300

    Update 3-puts.c
    Put c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,13 @@
-#include "main.h"
-
-/**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: pointer to the string to print
- *
- * Return: void
- */
-void _puts(char *str)
-{
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
-}
+#include "main.h" 
+ /** 
+  * _puts - prints a string, followed by a new line, to stdout 
+  * @str: string to print 
+  */ 
+ void _puts(char *str) 
+ { 
+         while (*str != '\0') 
+         { 
+                 _putchar(*str++); 
+         } 
+         _putchar('\n'); 
+ }
