040e28d1a87db619d79bbe2a46137f59e9c02b16
owinoalfred <ogzialfred@gmail.com>
2023-03-27 08:49:42 -0700

    lenbnth of t he

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: The string to be printed.
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
