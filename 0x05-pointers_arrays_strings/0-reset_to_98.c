b99955f8d326dc66c0fe54bdeafbf921db63dec7
owinoalfred <ogzialfred@gmail.com>
2023-03-27 08:37:07 -0700

    level 1 qn 1pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of an integer to 98.
+ * @n: A pointer to an integer.
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
