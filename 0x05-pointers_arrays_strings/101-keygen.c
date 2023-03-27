dff387f2fe16ee300ba59293fc49b1482ba64f91
owinoalfred <ogzialfred@gmail.com>
2023-03-27 10:45:01 -0700

    lenbnth of t he

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_LEN 62 // The number of characters that can be used in the password
-#define PASSWORD_LEN 22 // The length of the password
+#define MAX_LEN 62
+#define PASSWORD_LEN 22
 
 /**
  * main - Entry point
@@ -12,22 +12,19 @@
  */
 int main(void)
 {
-    char password[PASSWORD_LEN + 1]; // Initialize the password buffer (+1 for the null terminator)
-    const char *chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // Characters that can be used in the password
-    int i;
-
-    srand(time(NULL)); // Seed the random number generator with the current time
-
-    // Generate a random password
+    char password[PASSWORD_LEN + 1];
+const char *chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // Characters that can be used in the password
+int i;
+    srand(time(NULL));
     for (i = 0; i < PASSWORD_LEN; i++)
     {
-        int idx = rand() % MAX_LEN; // Generate a random index into the chars array
-        password[i] = chars[idx]; // Append the selected character to the password buffer
+        int idx = rand() % MAX_LEN;
+        password[i] = chars[idx];
     }
 
-    password[PASSWORD_LEN] = '\0'; // Add a null terminator to the end of the password buffer
+    password[PASSWORD_LEN] = '\0';
 
-    printf("%s", password); // Print the password to stdout
+    printf("%s", password);
 
     return (0);
 }
