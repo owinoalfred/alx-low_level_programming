dbe96661daf11923fcbb0c60ee7f6f11ebad6602
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-27 23:22:56 +0300

    Update 100-atoi.c
    Changed 101

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,35 +2,23 @@
 
 int _atoi(char *s)
 {
-    int sign = 1;
-    int num = 0;
-    int started = 0;
-
-    while (*s != '\0') {
-        if (*s == '-' || *s == '+') {
-            sign = (*s == '-') ? -1 : 1;
-        } else if (*s >= '0' && *s <= '9') {
-            started = 1;
-            int digit = *s - '0';
-
-            if (num == INT_MIN / 10 && digit > 8 && sign == -1) {
-                /* we have reached the minimum value for a signed integer */
+    int sign = 1, digit, result = 0;
+    while (*s != '\0')
+    {
+        if (*s == '-')
+            sign *= -1;
+        if (*s >= '0' && *s <= '9')
+        {
+            digit = *s - '0';
+            result = result * 10 + sign * digit;
+            if (result > 0 && sign < 0)
                 return INT_MIN;
-            }
-
-            /* compute the next value */
-            num = num * 10 - digit;
-
-            if (num > 0) {
-                /* integer overflow occurred */
-                return (sign == -1) ? INT_MIN : INT_MAX;
-            }
-        } else if (started) {
-            /* we reached the end of the number */
-            break;
+            else if (result < 0 && sign > 0)
+                return INT_MAX;
         }
+        else if (result != 0)
+            break;
         s++;
     }
-
-    return sign *num;
+    return result;
 }
