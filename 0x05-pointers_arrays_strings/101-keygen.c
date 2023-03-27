b67bc4fca50bce927ba531f937abc02774c93bbb
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-28 00:04:09 +0300

    Update 101-keygen.c
    11f

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,36 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 63
+#define PASSWORD_LENGTH 15
 
 /**
  * main - Generates a random valid password for 101-crackme
  *
  * Return: 0 on success
  */
+
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];
-    int i, random_value;
+  char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}\\|;:'\",.<>/?`~";
+  char password[PASSWORD_LENGTH + 1];
+  int i;
 
-    srand(time(NULL));
+  srand(time(NULL));
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        random_value = rand() % 62;
-        if (random_value < 26) {
-            /* lowercase letter */
-            password[i] = 'a' + random_value;
-        } else if (random_value < 52) {
-            /* uppercase letter */
-            password[i] = 'A' + random_value - 26;
-        } else {
-            /* digit */
-            password[i] = '0' + random_value - 52;
-        }
-    }
-    password[PASSWORD_LENGTH] = '\0';
+  for (i = 0; i < PASSWORD_LENGTH; i++) {
+    password[i] = charset[rand() % sizeof(charset)];
+  }
 
-    printf("%s", password);
+  password[PASSWORD_LENGTH] = '\0';
 
-    return 0;
+  printf("%s\n", password);
+
+  return 0;
 }
