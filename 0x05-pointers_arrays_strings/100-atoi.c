e21b42bdf1ffdbb5594adbd36e6d7ca391b0e69a
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-27 23:19:31 +0300

    Update 100-atoi.c
    1011

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,36 @@
-#include "main.h"
-/**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: The integer value of the converted string.
- */
+#include <limits.h>
+
 int _atoi(char *s)
 {
-int sign = 1;
-int num = 0;
-int started = 0;
-while (*s != '\0')
-{
-if (*s == '-' || *s == '+') {
-sign = (*s == '-') ? -1 : 1;
-} 
-else if (*s >= '0' && *s <= '9') {
-started = 1;
-int digit = *s - '0';
-if (num == INT_MIN / 10 && digit > 8 && sign == -1)
-{
-return INT_MIN;
-}
-num = num * 10 - digit;
-if (num > 0)
-{
-return (sign == -1) ? INT_MIN : INT_MAX;
-}
-}
-else if (started) {
-break;
-}
-s++;
-}
-return (sign *num);
+    int sign = 1;
+    int num = 0;
+    int started = 0;
+
+    while (*s != '\0') {
+        if (*s == '-' || *s == '+') {
+            sign = (*s == '-') ? -1 : 1;
+        } else if (*s >= '0' && *s <= '9') {
+            started = 1;
+            int digit = *s - '0';
+
+            if (num == INT_MIN / 10 && digit > 8 && sign == -1) {
+                /* we have reached the minimum value for a signed integer */
+                return INT_MIN;
+            }
+
+            /* compute the next value */
+            num = num * 10 - digit;
+
+            if (num > 0) {
+                /* integer overflow occurred */
+                return (sign == -1) ? INT_MIN : INT_MAX;
+            }
+        } else if (started) {
+            /* we reached the end of the number */
+            break;
+        }
+        s++;
+    }
+
+    return sign *num;
 }
