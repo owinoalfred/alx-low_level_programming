f9eca8ea61276fc2d0b327160dc3e8f7c9bc901a
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-27 23:17:34 +0300

    Update 100-atoi.c
    101

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,19 +7,31 @@
  */
 int _atoi(char *s)
 {
-int sign = 1, num = 0, started = 0;
-while (*s)
+int sign = 1;
+int num = 0;
+int started = 0;
+while (*s != '\0')
 {
-if (*s == '-')
-sign *= -1;
-if (*s >= '0' && *s <= '9')
-{
-num = (num * 10) + (*s - '0');
+if (*s == '-' || *s == '+') {
+sign = (*s == '-') ? -1 : 1;
+} 
+else if (*s >= '0' && *s <= '9') {
 started = 1;
+int digit = *s - '0';
+if (num == INT_MIN / 10 && digit > 8 && sign == -1)
+{
+return INT_MIN;
 }
-else if (started)
+num = num * 10 - digit;
+if (num > 0)
+{
+return (sign == -1) ? INT_MIN : INT_MAX;
+}
+}
+else if (started) {
 break;
+}
 s++;
 }
-return (num *sign);
+return (sign *num);
 }
