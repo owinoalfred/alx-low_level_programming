7939ea59583b008ff64ccfc5970b243b80a163f9
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-27 23:27:58 +0300

    Update 100-atoi.c
    Finito

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,23 +9,23 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1, digit, result = 0;
-    while (*s != '\0')
-    {
-        if (*s == '-')
-            sign *= -1;
-        if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
-            result = result * 10 + sign * digit;
-            if (result > 0 && sign < 0)
-                return INT_MIN;
-            else if (result < 0 && sign > 0)
-                return INT_MAX;
-        }
-        else if (result != 0)
-            break;
-        s++;
-    }
-    return result;
+int sign = 1, digit, result = 0;
+while (*s != '\0')
+{
+if (*s == '-')
+sign *= -1;
+if (*s >= '0' && *s <= '9')
+{
+digit = *s - '0';
+result = result * 10 + sign * digit;
+if (result > 0 && sign < 0)
+return INT_MIN;
+else if (result < 0 && sign > 0)
+return INT_MAX;
+}
+else if (result != 0)
+break;
+s++;
+}
+return result;
 }
