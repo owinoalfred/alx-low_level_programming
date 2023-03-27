6fee3f7410db899516bd24da5563e127be1de2cf
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-03-27 23:24:41 +0300

    Update 100-atoi.c
    Betty style

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,12 @@
 #include <limits.h>
 
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s: the string to convert
+ *
+ * Return: the integer value represented by the string, or 0 if no number is found
+ */
 int _atoi(char *s)
 {
     int sign = 1, digit, result = 0;
