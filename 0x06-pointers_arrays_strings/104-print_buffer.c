5321553f65d9dcd0f0c986efe40a21d351f70384
owinoalfred <ogzialfred@gmail.com>
2023-03-30 07:06:24 -0700

    function Write a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,19 +16,30 @@ for (i = 0; i < size; i += 10) {
 printf("%08x: ", i);
 for (j = 0; j < 10; j++) {
 if (i + j < size) {
-printf("%02x", *(b + i + j));
-} else {
+printf("%02x", b[i+j]);
+}
+else
+{
 printf("  ");
 }
-if (j % 2) {
+if (j % 2 == 1) {
 printf(" ");
 }
 }
 for (j = 0; j < 10; j++) {
-if (i + j >= size) {
-break;
+if (i + j < size) {
+if (b[i+j] >= 32 && b[i+j] <= 126) {
+printf("%c", b[i+j]);
 }
-printf("%c", *(b + i + j) >= ' ' && *(b + i + j) <= '~' ? *(b + i + j) : '.');
+else
+{
+printf(".");
+}
+}
+else
+{
+printf(" ");
+}
 }
 printf("\n");
 }
