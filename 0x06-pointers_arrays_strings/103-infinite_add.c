3055a28c82e93a060fbe8ef9c85c3a80cac5cbb3
owinoalfred <ogzialfred@gmail.com>
2023-03-30 11:18:28 -0700

    function Write a function that prints a buffer.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,32 +1,40 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer to store result
- * @size_r: size of buffer
- * Return: pointer to result
+ * infinite_add - adds two numbers stored as strings
+ * @n1: the first number
+ * @n2: the second number
+ * @r: the buffer where the result is stored
+ * @size_r: the size of the buffer
+ *
+ * Return: a pointer to the result, or 0 if it can't be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l1, l2, sum, carry = 0;
-for (l1 = 0; n1[l1]; l1++)
-;
-for (l2 = 0; n2[l2]; l2++)
-;
-if (l1 + l2 + 1 > size_r)
+int len1 = 0, len2 = 0, i, j, k, carry = 0;
+while (n1[len1])
+len1++;
+while (n2[len2])
+len2++;
+if (len1 + len2 + 1 > size_r)
 return (0);
-for (i = l1 - 1, j = l2 - 1, k = 0; i >= 0 || j >= 0 || carry; i--, j--, k++)
+for (i = len1 - 1, j = len2 - 1, k = 0; k < size_r - 1 && (i >= 0 || j >= 0); i--, j--, k++)
 {
-sum = carry;
+int sum = carry;
 if (i >= 0)
 sum += n1[i] - '0';
 if (j >= 0)
 sum += n2[j] - '0';
+r[k] = (sum % 10) + '0';
 carry = sum / 10;
-r[k] = sum % 10 + '0';
 }
+if (carry)
+{
+if (k >= size_r - 1)
+return (0);
+r[k++] = carry + '0';
+}
+r[k] = '\0';
 for (i = 0, j = k - 1; i < j; i++, j--)
 {
 char tmp = r[i];
