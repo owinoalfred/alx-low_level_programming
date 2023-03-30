f9e61e2b102751b0bda026a43d1183e784f79104
owinoalfred <ogzialfred@gmail.com>
2023-03-30 06:58:04 -0700

    function that function that prints an integer..

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,35 @@
-include "main.h";
+include "main.h"
 /**
-
-infinite_add - adds two numbers stored as strings
-
-@n1: first number to add
-
-@n2: second number to add
-
-@r: buffer to store the result
-
-@size_r: size of the buffer
-
-Return: pointer to the result or 0 if the result cannot be stored in r
-*/
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+ * print_buffer - prints the content of a buffer
+ * @b: the buffer to print
+ * @size: the size of the buffer
+ */
+void print_buffer(char *b, int size)
 {
-int len1 = 0, len2 = 0, carry = 0, sum = 0;
 int i, j;
-while (n1[len1])
-len1++;
-while (n2[len2])
-len2++;
-if (len1 + 2 > size_r || len2 + 2 > size_r)
-return (0);
-for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+if (size <= 0)
+printf("\n");
+for (i = 0; i < size; i += 10)
 {
-sum = carry;
-if (i >= 0)
-sum += n1[i] - '0';
-if (j >= 0)
-sum += n2[j] - '0';
-carry = sum / 10;
-r[i + 1] = sum % 10 + '0';
-}
-if (carry)
+printf("%08x: ", i);
+for (j = i; j < i + 10; j++)
 {
-if (len1 + 1 < size_r)
-r[0] = carry + '0';
+if (j < size)
+printf("%02x", *(b + j));
 else
-return (0);
+printf("  ");
+if (j % 2)
+printf(" ");
 }
-return (r);
+for (j = i; j < i + 10; j++)
+{
+if (j >= size)
+break;
+if (*(b + j) >= 32 && *(b + j) <= 126)
+printf("%c", *(b + j));
+else
+printf(".");
+}
+printf("\n");
+}
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,8 @@ char *rot13(char *);
 void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+void print_buffer(char *b, int size);
 char *string_toupper(char *);
 char *cap_string(char *);
 #endif /* MAIN_H */
