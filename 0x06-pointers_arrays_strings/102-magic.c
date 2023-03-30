d4caa6cda0a315690f3f4e055832d54befc3ee1a
owinoalfred <ogzialfred@gmail.com>
2023-03-30 06:13:26 -0700

    function that function that prints an integer..

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+main - Entry point
+Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;
@@ -7,7 +11,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-*(a + 2) = 98;
+*(p + 5) = 98; /* add this line */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
