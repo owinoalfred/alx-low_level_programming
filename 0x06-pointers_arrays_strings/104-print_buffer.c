3ac92c17fe5f9b535c993c6156c4b5e3326ab194
owinoalfred <ogzialfred@gmail.com>
2023-03-30 12:03:42 -0700

    function Write a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,51 +1,40 @@
-include "main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - prints the content of a buffer
- * @b: the buffer to print
- * @size: the size of the bufer
+ * print_buffer - prints a buffer.
+ *
+ *@b: the buffer
+ *@size: size of the buffer
+ *
  */
-
 void print_buffer(char *b, int size)
 {
-int i, j;
-if (size <= 0)
-{
-printf("\n");
-return;
-}
-for (i = 0; i < size; i += 10)
-{
-printf("%08x: ", i);
-for (j = 0; j < 10; j++)
-{
-if (i + j < size)
-{
-printf("%02x", *(b + i + j));
-}
-else
-{
-printf("  ");
-}
-if (j % 2)
-{
-printf(" ");
-}
-}
-for (j = 0; j < 10; j++)
-{
-if (i + j < size)
-{
-if (*(b + i + j) >= 32 && *(b + i + j) <= 126)
-{
-printf("%c", *(b + i + j));
-}
-else
-{
-printf(".");
-}
-}
-}
-printf("\n");
-}
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j += 2)
+		{
+			if (i + j < size && i + j + 1 < size)
+				printf("%02x%02x ", b[i + j], b[i + j + 1]);
+			else if (i + j < size)
+				printf("%02x   ", b[i + j]);
+			else
+				printf("     ");
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j >= size)
+				break;
+			if (b[i + j] > 31 && b[i + j] <= 126)
+				printf("%c", b[i + j]);
+			else
+				 printf(".");
+		}
+		printf("\n");
+	}
+	if (size <= 0)
+		printf("\n");
 }
