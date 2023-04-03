121cff1ab12a43481ba0885e534ef1e0f49b21f1
owinoalfred <ogzialfred@gmail.com>
2023-04-03 06:39:50 -0700

    this is the one where i fix a betty mistake

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,8 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    char *p = s;
-
-    while (n--)
-        *p++ = b;
-
-    return (s);
+char *p = s;
+while (n--)
+*p++ = b;
+return (s);
 }
-
