8328eade5c93281e01cff0f2bd9b2af191f75bbc
owinoalfred <ogzialfred@gmail.com>
2023-04-11 09:52:14 -0700

    betty is still t he issue

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@
  * 2.  Next, we loop through the string to find the length of the string.
  * 3.  We allocate memory for the copy of the string.
  * 4.  We check if the memory was allocated.  If it wasn’t, we return NULL.
- * 5.  We loop through the string again, this time copying the string into the new memory.
+ * 5.  We loop through the string again, this timing into the new memory.
  * 6.  We add a null terminator to the end of the string.
  * 7.  We return the copy of the string.
  *
