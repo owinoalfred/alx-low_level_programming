6f123889a0228e54a04e14100b91dc4926446a03
owinoalfred <ogzialfred@gmail.com>
2023-04-11 09:47:46 -0700

    adding betty style documentation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,16 +7,13 @@
  *@c: Character to insert
  *Return: NULL if size is zero or if it fails,
  *pointer to array if everything is normal.
-
- *FUNCTIONALITY *
-
+ *FUNCTIONALITY
  * 1. First, we check if the size is 0. If it is, we return NULL.
  * 2. Then, we allocate memory for the array.
  * 3. If the allocation fails, we return NULL.
- * 4. If the allocation succeeds, we loop through the array and set each element to the value of c.
+ * 4. If the alld set each element to the value of c.
  * 5. Finally, we return the array.
  * Time Complexity: O(n)
-
 */
 char *create_array(unsigned int size, char c)
 {
