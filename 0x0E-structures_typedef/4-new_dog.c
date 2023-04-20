7eb8beb9f920d13e8f478a31d71fa4841a30cf99
owinoalfred <ogzialfred@gmail.com>
2023-04-20 13:01:57 -0700

    firnal

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,21 +50,17 @@ return (dest);
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *doggo;
-
 if (name == NULL || age < 0 || owner == NULL)
 return (NULL);
-
 doggo = malloc(sizeof(dog_t));
 if (doggo == NULL)
 return (NULL);
-
 doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
 if (doggo->name == NULL)
 {
 free(doggo);
 return (NULL);
 }
-
 doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 if (doggo->owner == NULL)
 {
@@ -72,10 +68,8 @@ free(doggo->name);
 free(doggo);
 return (NULL);
 }
-
 doggo->name = _strcopy(doggo->name, name);
 doggo->age = age;
 doggo->owner = _strcopy(doggo->owner, owner);
-
 return (doggo);
 }
