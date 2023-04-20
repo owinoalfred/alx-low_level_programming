d180158f3877d7fe20cb887cffd7e410b989325e
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-04-20 23:07:51 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -18,5 +18,20 @@ void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
+struct list
+{
+	char chars;
+	void (*f)(va_list);
+};
+typedef struct list char_list;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void c_print(va_list pars);
+void i_print(va_list pars);
+void f_print(va_list pars);
+void s_print(va_list pars);
+
 
 #endif
