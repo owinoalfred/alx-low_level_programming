5531d4b9b3679ac61c6b3746765552e74fc50ed6
Alfred owino <59254825+owinoalfred@users.noreply.github.com>
2023-04-28 23:42:18 +0300

    Update 0-print_list.c
    from hasa

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,24 @@
 #include "lists.h"
 
 /**
-* print_list - prints all the elements of a list
-*
-* @h:pointer to the list
-*
-*Return: the number of nodes
-*/
+ * print_list - prints all the elements of a list_t list.
+ * @h: list
+ *
+ * Return: number of nodes
+ */
 
 size_t print_list(const list_t *h)
 {
-const list_t *cursor = h;
-size_t count = 0;
-
-while (cursor != NULL)
+int i;
+for (i = 0; h; h = h->next, i++)
 {
-if (cursor->str != NULL)
-printf("[%d] %s\n", cursor->len, cursor->str);
-else
+if (!h->str)
 printf("[0] (nil)\n");
-count += 1;
-cursor = cursor->next;
+else
+{
+printf("[%u] ", h->len);
+printf("%s\n", h->str);
 }
-
-return (count);
+}
+return (i);
 }
